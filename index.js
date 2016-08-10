61069f4700a774a661e9482f29ca7ca269d5bade
Eric McNiece <emcniece@gmail.com>
2016-08-10 15:45:34 -0700

    Testing layered videos

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var omx = require('./node-omxplayer');
 var Mouse = require("node-mouse");
+var debounce = require('debounce');
+var spawn = require('child_process').spawn;
  
-var player = omx();
-var m = new Mouse();
-var seqState = 0;
 var volume = 20; // volume in millibels
-var omxArgs = ['--no-osd', '--blank'];
+var player = omx();
+var bgPlayer = omx(getVideo(0), 'both', true, volume, ['--no-osd', '--blank', '--layer', '1']);
+var m = new Mouse();
+var seqState = 1;
+var omxArgs = ['--no-osd', '--blank', '--layer', '2'];
 
 function getVideo(index){
-
   var videos = [
     'Space_Hand_BG.mov',
     'SpaceHandS1.mov',
@@ -28,31 +30,39 @@ function playCurrentVideo(){
   player.newSource(getVideo(seqState), 'both', loop, volume, omxArgs);
 }
 
-playCurrentVideo(seqState);
-
 // Events: mousedown, mouseup, click, mousemove
-m.on("mousedown",function(event) {
-  if(seqState++ >= 5) seqState = 0;
-  console.log('Clicked!', seqState);
+m.on('mousedown', debounce(incrementVideo, 1000, true));
 
-  playCurrentVideo(seqState);
-});
+function incrementVideo(){
+  bgPlayer.prevChapter();
+
+  if(seqState++ >= 5) seqState=1;
+  console.log('Clicked!', seqState);
+  playCurrentVideo();
+}
 
 player.on('close', function(){  
   console.log('Video end');
-  seqState = 0;
-  playCurrentVideo(seqState);
+  seqState = 1;
+
+  bgPlayer.prevChapter();
 });
+
+player.on('error', console.log);
 
 process.stdin.resume();
 
 function exitHandler(options, err) {
   console.info('Handling exit...');
+  process.stdin.end();
 
-  if(player && player.running){
-    player.quit();
-    console.log('OMXPlayer shutdown success');
-  }
+  if(player.running) player.quit();
+  if(bgPlayer.running) bgPlayer.quit();
+
+  console.log('OMXPlayer shutdown success');
+
+  spawn('killall', ['omxplayer', 'omxplayer.bin', 'node', '-s', '9']);
+
   if (options.cleanup) console.log('clean');
   if (err) console.log(err.stack);
   if (options.exit) process.exit();
--- test-queue.js
+++ test-queue.js
@@ -1,6 +1,10 @@
-var omx = require('node-omxplayer');
+var fs = require('fs');
+var jsonfile = require('jsonfile');
+var util = require('util');
+var omx = require('./node-omxplayer');
 var Mouse = require("node-mouse");
- 
+
+var dFile = './debug.json';
 var player1 = omx();
 var player2 = omx();
 var m = new Mouse();
@@ -16,33 +20,41 @@ var videos = [
 ];
 
 var prefix = __dirname + '/videos/';
-player1.newSource(prefix+videos[0], 'both', true, volume, ['--no-osd']);
+player1.newSource(prefix+videos[0], 'both', true, volume /*, ['--no-osd']*/);
 //player2.newSource(prefix+videos[3], 'both', false, volume);
 
 // Events: mousedown, mouseup, click, mousemove
 m.on("mousedown",function(event) {
   if(event.leftBtn){
-    player1.pause();
-    console.log('paused');
+    //player1.pause();
+    player1.prevChapter();
+    //player1.pause();
+    console.log('prevChapter pause');
   } else if(event.rightBtn){
     player1.play();
     console.log('playing');
   }
-  player1.prevChapter();
 });
 
 player1.on('close', function(){  
   console.log('Video 1 end');
 });
 
-player2.on('close', function(){
-  console.log('Video 2 end');
+player1.on('error', function(err){
+  console.log('Error', err);
 });
+
+fs.writeFile('test.txt', 'wtf');
 
 process.stdin.resume();
 
 function exitHandler(options, err) {
   console.info('Handling exit...');
+
+  console.log( util.inspect(process._getActiveHandles()) );
+
+  process.stdin.end();
+  process.stdin.unref();
 
   if(player1 && player1.running){
     player1.quit();
@@ -50,7 +62,8 @@ function exitHandler(options, err) {
   }
   if (options.cleanup) console.log('clean');
   if (err) console.log(err.stack);
-  if (options.exit) process.exit();
+  //if (options.exit)
+     process.exit();
 }
 
 //do something when app is closing
